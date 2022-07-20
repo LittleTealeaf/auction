bda5a8e20e55f9eb478d80667844b7095b304ed4
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-07-20 17:42:49 -0400

    changes

--- pages/api/customer.ts
+++ pages/api/customer.ts
@@ -10,27 +10,25 @@ export default async function handler(req: NextApiRequest, res: Response) {
     }
 }
 
-type GetQueryParameters = {
-    id?: number;
-    firstName?: string;
-    lastName?: string;
-    phone?: string;
-    email?: string;
-    address?: string;
-};
-
 async function GET(req: NextApiRequest, res: Response) {
-    const query: GetQueryParameters = req.query;
+    const query: {
+        id?: number;
+        firstName?: string;
+        lastName?: string;
+        phone?: string;
+        email?: string;
+        address?: string;
+    } = req.query;
 
     //If the user specifies and ID, perform a singular id lookup
-    if(query.id != null) {
+    if (query.id != null) {
         const result = await db.customer.findFirst({
             where: {
-                id: query.id
-            }
+                id: query.id,
+            },
         });
 
-        if(result == null) return res.status(204).end();
+        if (result == null) return res.status(204).end();
 
         return res.status(200).json(result);
     }
@@ -42,11 +40,15 @@ async function GET(req: NextApiRequest, res: Response) {
             lastName: containsQuery(query.lastName),
             phone: containsQuery(query.phone),
             address: containsQuery(query.address),
-            email: containsQuery(query.email)
-        }
-    })
+            email: containsQuery(query.email),
+        },
+    });
 
-    if(result.length == 0) return res.status(204).end();
+    if (result.length == 0) return res.status(204).end();
 
     return res.status(200).json(result);
+}
+
+async function POST(req: NextApiRequest, res: Response) {
+    
 }
