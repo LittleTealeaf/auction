c0aa83ccb3b6cdfc2c2e2397ec28b915d5e48870
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-07-21 15:07:36 +0000

    changes

--- pages/api/customer.ts
+++ pages/api/customer.ts
@@ -15,10 +15,15 @@ async function GET(req: NextApiRequest, res: Response) {
     const query: {
         id?: number;
         query?: string[];
+        count?: number;
     } = req.query;
 
+    if(query.count == null) {
+        query.count = 1000;
+    }
+
     if(query.id != null) {
-        const result = await db.customer.findFirst({
+        const result = await db.customer.findUnique({
             where: {
                 id: query.id
             }
@@ -33,7 +38,7 @@ async function GET(req: NextApiRequest, res: Response) {
     }
 
     if(query.query == null) {
-        const result = await db.customer.findMany();
+        const result = await db.customer.findMany({take: query.count});
 
         return res.status(200).json(result);
     }
