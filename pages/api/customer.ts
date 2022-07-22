14bfb2254e2fe776602d3638aa85e88dcca53d5a
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-07-22 13:37:12 +0000

    Update PUT method

--- pages/api/customer.ts
+++ pages/api/customer.ts
@@ -5,10 +5,17 @@ import { NextApiRequest, NextApiResponse } from "next";
 type Response = NextApiResponse<Customer[] | Customer | null>;
 
 export default async function handler(req: NextApiRequest, res: Response) {
+    // if (req.method == "GET") return GET(req, res);
+    // if (req.method == "POST") return POST(req, res);
     if (req.method == "GET") return GET(req, res);
+    if (req.method == "PUT") return PUT(req, res);
     if (req.method == "POST") return POST(req, res);
+    if (req.method == "DELETE") return DELETE(req, res);
+
+    res.status(405).end();
 }
 
+//Get
 async function GET(req: NextApiRequest, res: Response) {
     //TODO: Change this so theres a "query" term that just searches for matches in all categories
 
@@ -18,18 +25,18 @@ async function GET(req: NextApiRequest, res: Response) {
         count?: number;
     } = req.query;
 
-    if(query.count == null) {
+    if (query.count == null) {
         query.count = 1000;
     }
 
-    if(query.id != null) {
+    if (query.id != null) {
         const result = await db.customer.findUnique({
             where: {
-                id: query.id
-            }
-        })
+                id: query.id,
+            },
+        });
 
-        if(result == null) {
+        if (result == null) {
             res.status(204).end();
             return;
         }
@@ -37,71 +44,57 @@ async function GET(req: NextApiRequest, res: Response) {
         return res.status(200).json(result);
     }
 
-    if(query.query == null) {
-        const result = await db.customer.findMany({take: query.count});
+    if (query.query == null) {
+        const result = await db.customer.findMany({ take: query.count });
 
         return res.status(200).json(result);
     }
-
-
-
-    // const query: {
-    //     id?: number;
-    //     firstName?: string;
-    //     lastName?: string;
-    //     phone?: string;
-    //     email?: string;
-    //     address?: string;
-    // } = req.query;
-
-    // //If the user specifies and ID, perform a singular id lookup
-    // if (query.id != null) {
-    //     const result = await db.customer.findFirst({
-    //         where: {
-    //             id: query.id,
-    //         },
-    //     });
-
-    //     if (result == null) return res.status(204).end();
-
-    //     return res.status(200).json(result);
-    // }
-
-    // //Perform a full query
-    // const result = await db.customer.findMany({
-    //     where: {
-    //         firstName: containsQuery(query.firstName),
-    //         lastName: containsQuery(query.lastName),
-    //         phone: containsQuery(query.phone),
-    //         address: containsQuery(query.address),
-    //         email: containsQuery(query.email),
-    //     },
-    // });
-
-    // if (result.length == 0) return res.status(204).end();
-
-    // return res.status(200).json(result);
 }
 
-async function POST(req: NextApiRequest, res: Response) {
-
-    const data = req.query;
-
+//Update
+async function PUT(req: NextApiRequest, res: Response) {
     if (req.query.id == null) {
-        // Create and update req query id
-        const new_customer = await db.customer.create({
-            data: {},
-        });
-
-        data.id = String(new_customer.id);
     }
+
+    const id = Number(req.query.id);
+    const data = req.query;
+    delete data.id;
 
     const result = await db.customer.update({
         where: {
-            id: Number(req.query.id),
+            id,
         },
-        data
+        data,
     });
-
-    return res.status(200).json(result);
 }
+
+//Create
+async function POST(req: NextApiRequest, res: Response) {}
+
+//Delete
+async function DELETE(req: NextApiRequest, res: Response) {}
+
+// async function POST_old(req: NextApiRequest, res: Response) {
+
+//     const data = req.query;
+
+//     if (req.query.id == null) {
+//         // Create and update req query id
+//         const new_customer = await db.customer.create({
+//             data: {},
+//         });
+
+//         data.id = String(new_customer.id);
+//     }
+
+//     const result = await db.customer.update({
+//         where: {
+//             id: Number(req.query.id),
+//         },
+//         data
+//     });
+
+//     return res.status(200).json(result);
+// }
+
+//
