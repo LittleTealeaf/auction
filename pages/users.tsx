794155bc7dc01b8d50aced6d37f26c71a79fc46c
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-07-28 14:15:19 +0000

    Updates

--- pages/users.tsx
+++ pages/users.tsx
@@ -5,38 +5,38 @@ import { fetchAuth, fetchEffect } from "lib/fetch";
 import { useEffect, useState } from "react";
 import { useAuthContext } from "./_app";
 
-function UserTable({}: {}) {
-    const [users, setUsers] = useState<User[] | undefined>(undefined);
-    const [permissions, setPermissions] = useState<Permissions[] | undefined>(undefined);
+// function UserTable({}: {}) {
+//     const [users, setUsers] = useState<User[] | undefined>(undefined);
+//     const [permissions, setPermissions] = useState<Permissions[] | undefined>(undefined);
 
-    useEffect(() => fetchEffect(setUsers, "user", {}), []);
+//     useEffect(() => fetchEffect(setUsers, "users", {}), []);
 
-    return (
-        <Table>
-            <TableHead>
-                <TableCell>Username</TableCell>
-                <TableCell>Password</TableCell>
-                <TableCell></TableCell>
-            </TableHead>
-            <TableBody>
-                {users === undefined ? (
-                    <>
-                        <TableCell>
-                            <Skeleton variant="text"></Skeleton>
-                        </TableCell>
-                    </>
-                ) : (
-                    users.map((user) => (
-                        <>
-                            <TableCell>{user.username}</TableCell>
-                            <TableCell>{user.password}</TableCell>
-                        </>
-                    ))
-                )}
-            </TableBody>
-        </Table>
-    );
-}
+//     return (
+//         <Table>
+//             <TableHead>
+//                 <TableCell>Username</TableCell>
+//                 <TableCell>Password</TableCell>
+//                 <TableCell></TableCell>
+//             </TableHead>
+//             <TableBody>
+//                 {users === undefined ? (
+//                     <>
+//                         <TableCell>
+//                             <Skeleton variant="text"></Skeleton>
+//                         </TableCell>
+//                     </>
+//                 ) : (
+//                     users.map((user) => (
+//                         <>
+//                             <TableCell>{user.username}</TableCell>
+//                             <TableCell>{user.password}</TableCell>
+//                         </>
+//                     ))
+//                 )}
+//             </TableBody>
+//         </Table>
+//     );
+// }
 
 export default function Page({}) {
     const [permissions, setPermissions] = useState<{ manageUser: boolean } | null>(null);
@@ -49,5 +49,5 @@ export default function Page({}) {
 
     if (!permissions.manageUser) return <ForbiddenPage />;
 
-    return <UserTable />;
+    return <></>;
 }
