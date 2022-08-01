d9a8679cf6a11df03d5c887df28b039a05ed0535
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-08-01 15:20:24 -0400

    Started working on app nav

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,13 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import type { AppProps } from "next/app";
-import { useEffect, useState } from "react";
+import { MouseEventHandler, useEffect, useState } from "react";
 import { UserData } from "types/api";
 import LoadingPage from "components/screen/loading";
 import LoginPage from "components/screen/login";
 import { fetchAPI } from "lib/app/fetch";
+import { AppBar, Box, Container, Drawer, IconButton, List, Menu, Toolbar } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function AppRoot({ Component, pageProps }: AppProps) {
     const [user, setUser] = useState<UserData | null | undefined>(undefined);
@@ -39,8 +41,37 @@ function AppRoot({ Component, pageProps }: AppProps) {
 }
 
 function AppPage({ Component, pageProps, user }: AppProps & { user: UserData }) {
+
+    const [showDrawer, setShowDrawer] = useState(false);
+
+    const handleOpenNavMenu: MouseEventHandler<HTMLButtonElement> = (event) => {
+        setShowDrawer(!showDrawer);
+    };
+
     return (
         <>
+            {/* <AppBar position="static">
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <Box
+                            sx={{
+                                flexGrow: 1,
+                                display: {
+                                    xs: "flex",
+                                    md: "none",
+                                },
+                            }}
+                        >
+                            <IconButton size="large" aria-label="open menu" color="inherit" aria-haspopup="true" aria-controls="menu-appbar" onClick={handleOpenNavMenu}>
+                                <MenuIcon />
+                            </IconButton>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+            <Drawer open={showDrawer} onClose={() => setShowDrawer(false)}>
+                <List></List>
+            </Drawer> */}
             <Component {...{ ...pageProps, user }} />
         </>
     );
