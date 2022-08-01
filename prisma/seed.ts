2d1e1c3da4abf704ded1b2930722189427d6c3e6
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-08-01 13:47:59 +0000

    removed hash in seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-import { hash } from "argon2";
+
 
 const db = new PrismaClient();
 
 Promise.all([
-    hash("admin").then((password) =>
-        db.user.create({
-            data: {
-                username: "admin",
-                password: 'admin',
-                manageUsers: true,
-            },
-        })
-    ),
+    db.user.create({
+        data: {
+            username: "admin",
+            password: 'admin',
+            manageUsers: true,
+        },
+    })
 ]);
